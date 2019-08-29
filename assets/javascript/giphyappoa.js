b8c282caf754e1533a42982f6ffc23048712e9b7
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-08-29 11:49:24 -0500

    created js file and array of 10 sports

---
+++ assets/javascript/giphyappoa.js
@@ -0,0 +1,8 @@
+$(document).ready (function(){
+
+//we have to use var not const because we want to add sports later in the array
+var sports = ["baseball","basketball","boxing","cycling","football","handball","golf","rugby","soccer","tennis"]
+
+
+
+}
