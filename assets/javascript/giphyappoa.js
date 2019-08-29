3e6335f254f4f9433e4d3e3aa9b5defc6b9b1275
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-08-29 12:33:23 -0500

    working on each sports button

--- assets/javascript/giphyappoa.js
+++ assets/javascript/giphyappoa.js
@@ -3,6 +3,27 @@ $(document).ready (function(){
 //we have to use var not const because we want to add sports later in the array
 var sports = ["baseball","basketball","boxing","cycling","football","handball","golf","rugby","soccer","tennis"]
 
+function Sportsgifs() {
+//not sure were to specify limit 10.I think it is ten by default
+let queryurl = "https://api.giphy.com/v1/gifs/search?q="+sport+"wAwF7AiQJSREASGcU6UxYjhfA2gpSxVq";
+const sport = $(this).attr("data-name")}
 
 
+$ajax ({url:queryurl, method:"GET"})
+        .done(function(response)
+        
+        {
+            $("#sportstopics").empty();
+            var answers=response.data;         
+        
+
+
+for (let jj =0; jj<10; jj++)
+    {
+       
+    var answer =("<img>")
+    answer.attr("data-name", answers[jj])
+    $("#sportstopics").append(answer);
 }
+
+})})
