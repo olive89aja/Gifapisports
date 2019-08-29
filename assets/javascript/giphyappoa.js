7a900255e10fd99144060af728e26e8e50f00e89
OLIVIER AUBERGER <oliviera89@gmail.com>
2019-08-29 14:21:09 -0500

    Ajax request working for one sport, not each sport in array. Wondering why

--- assets/javascript/giphyappoa.js
+++ assets/javascript/giphyappoa.js
@@ -1,19 +1,19 @@
 $(document).ready (function(){
 
 //we have to use var not const because we want to add sports later in the array
-var sports = ["baseball","basketball","boxing","cycling","football","handball","golf","rugby","soccer","tennis"]
+const sports = ["baseball","basketball","boxing","cycling","football","handball","golf","rugby","soccer","tennis"]
 
 Sportsgifs();
 
 function Sportsgifs() {
 //not sure were to specify limit 10.I think it is ten by default
-const sport = $(this).attr("data-name")
-let queryurl = "https://api.giphy.com/v1/gifs/search?q="+sport+"wAwF7AiQJSREASGcU6UxYjhfA2gpSxVq";
+const sport = $(this).attr("data-name");
+let queryurl = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&api_key=wAwF7AiQJSREASGcU6UxYjhfA2gpSxVq";
 
 
 
 $.ajax ({url:queryurl, method:"GET"})
-        .then(function(response)
+        .done(function(response)
         
         {
             // $("#sportsbuttons").empty();
@@ -25,7 +25,7 @@ for (let jj =0; jj<10; jj++)
     {
        
     var answer =("<div>")
-    answer.attr("sports-name",sports[jj])
+    $(answer).attr("sports-name",sports[jj])
     $("#sportsanswers").append(answer);
 
     var sportimage = ("<img>")
@@ -35,7 +35,7 @@ for (let jj =0; jj<10; jj++)
     }})
 
 
-buttonsWork();
+
 
 function buttonsWork() {
 
@@ -48,3 +48,4 @@ function buttonsWork() {
         $("#sportsanswers2").append(buttons);
     })
 }
+buttonsWork();
